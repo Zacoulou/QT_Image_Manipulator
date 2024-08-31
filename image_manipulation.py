54225fd11e952f6358b12d984670b50713d2f4e0
Zacoulou <isaac.abouaf@gmail.com>
2024-08-30 23:01:15 -0400

    Created skeleton for python script

---
+++ image_manipulation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+from PIL import Image
+
+def is_valid_path (path):
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(path + " is not a valid path")
+    return path
+
+def is_image(file_path):
+    try:
+        Image.open(file_path).verify()
+        return True
+    except (IOError, SyntaxError):
+        return False
+    
+def manipulateFilesFromPathToPath(source, dest):
+    for filename in os.listdir(source):
+        full_path = os.path.join(source, filename)
+        if is_image(full_path):
+            print(full_path)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Demo")
+    parser.add_argument("source_directory", type=is_valid_path)
+    parser.add_argument("destination_directory", type=is_valid_path)
+    args = parser.parse_args()
+
+    manipulateFilesFromPathToPath(args.source_directory, args.destination_directory)
